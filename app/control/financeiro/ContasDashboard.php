cfde50c690d6a53ae5fdeef4f37f37edaffa2cdd
edusmrs <edusmrs@hotmail.com>
2022-02-08 21:41:28 -0300

    Relatórios ContaPagar e ContaReceber

--- app/control/financeiro/ContasDashboard.php
+++ app/control/financeiro/ContasDashboard.php
@@ -1,18 +1,19 @@
 <?php
 
 use Adianti\Control\TPage;
-use Adianti\Database\TTransaction;
 use Adianti\Widget\Base\TElement;
-use Adianti\Widget\Container\TVBox;
-use Adianti\Widget\Template\THtmlRenderer;
+use Adianti\Widget\Container\TPanelGroup;
+use Adianti\Widget\Dialog\TMessage;
+use Adianti\Widget\Util\TXMLBreadCrumb;
 
-class ContasDashboard extends TPage{
+class ContasDashboard extends TPage
+{
     function __construct()
     {
         parent::__construct();
 
         $vbox = new TVBox;
-        $vbox->style = 'width> 100k';
+        $vbox->style = 'width: 100%';
 
         $div = new TElement('div');
         $div->class = "row";
@@ -21,38 +22,91 @@ class ContasDashboard extends TPage{
         {
             TTransaction::open('db_condominio');
 
-            $valor = ContaPagar::where('valor','>', 0)->sumBy('valor');
-            $valor_pago = ContaPagar::where('valor_pago','>', 0)->sumBy('valor_pago');
-            $juros = ContaPagar::where('saldo','>', 0)->sumBy('saldo');
+            $valor      = ContaPagar::where('valor', '>', 0)->sumBy('valor');
+            $valor_pago = ContaPagar::where('valor_pago', '>', 0)->sumBy('valor_pago');
+            $juros      = ContaPagar::where('saldo', '>', 0)->sumBy('saldo');
 
-            $valor_receber = ContaReceber::where('valor','>',0)->sumBy('valor');
-            $valor_recebido = ContaReceber::where('valor_recebido','>',0)->sumBy('valor_recebido');
-            $juros_recebido = ContaReceber::where('juros_recebido','>',0)->sumBy('juros_recebido');
+            $valor_receber  = ContaReceber::where('valor', '>', 0)->sumBy('valor');
+            $valor_recebido = ContaReceber::where('valor_recebido', '>', 0)->sumBy('valor_recebido');
+            $juros_recebido = ContaReceber::where('juros_recebido', '>', 0)->sumBy('juros_recebido');
 
             TTransaction::close();
 
-            $indicator1 = new THtmlRenderer('app/resources/info-box.html');
-            $indicator2 = new THtmlRenderer('app/resources/info-box.html');
-            $indicator3 = new THtmlRenderer('app/resources/info-box.html');
-            $indicator4 = new THtmlRenderer('app/resources/info-box.html');
-            $indicator5 = new THtmlRenderer('app/resources/info-box.html');
-            $indicator6 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador1 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador2 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador3 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador4 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador5 = new THtmlRenderer('app/resources/info-box.html');
+            $indicador6 = new THtmlRenderer('app/resources/info-box.html');
 
-            $indicator1->enableSection('main',['title'=>'VALOR A PAGAR','icon'=>'money-bill','background'=>'blue','value'=>'R$ '.number_format($valor,2,',','.')]);
-            $indicator2->enableSection('main',['title'=>'TOTAL A PAGAR','icon'=>'money-bill','background'=>'green','value'=>'R$ '.number_format($valor_pago,2,',','.')]);
-            $indicator3->enableSection('main',['title'=>'JUROS','icon'=>'money-bill','background'=>'orange','value'=>'R$ '.number_format($juros,2,',','.')]);
-            $indicator4->enableSection('main',['title'=>'VALOR A RECEBER','icon'=>'money-bill','background'=>'red','value'=>'R$ '.number_format($valor_receber,2,',','.')]);      
-            $indicator5->enableSection('main',['title'=>'VALOR RECEBIDO','icon'=>'money-bill','background'=>'pink','value'=>'R$ '.number_format($valor_recebido,2,',','.')]);
-            $indicator6->enableSection('main',['title'=>'JUROS RECEBIDO','icon'=>'money-bill','background'=>'orange','value'=>'R$ '.number_format($juros_recebido,2,',','.')]);
-        
-            $div->add(TElement::tag('div',$indicator1,['class'=>'col-se-6']));
-            $div->add(TElement::tag('div',$indicator2,['class'=>'col-se-6']));
-            $div->add(TElement::tag('div',$indicator3,['class'=>'col-se-6']));
-            $div->add(TElement::tag('div',$indicator4,['class'=>'col-se-6']));
-            $div->add(TElement::tag('div',$indicator5,['class'=>'col-se-6']));
-            $div->add(TElement::tag('div',$indicator6,['class'=>'col-se-6']));
+            $indicador1->enableSection('main', ['title' => 'VALOR A PAGAR', 'icon' => 'money-bill', 'background' => 'blue',
+                                                'value' => 'R$ '.number_format($valor, 2,',','.') ] );
+                                                
+            $indicador2->enableSection('main', ['title' => 'TOTAL A PAGAR', 'icon' => 'money-bill', 'background' => 'green',
+                                                'value' => 'R$ '.number_format($valor_pago, 2,',','.') ] );
+
+            $indicador3->enableSection('main', ['title' => 'JUROS', 'icon' => 'money-bill', 'background' => 'orange',
+                                                'value' => 'R$ '.$juros] );
+
+            $indicador4->enableSection('main', ['title' => 'VALOR A RECEBER', 'icon' => 'money-bill', 'background' => 'red',
+                                                'value' => 'R$ '.number_format($valor_receber, 2,',','.') ] );
+
+            $indicador5->enableSection('main', ['title' => 'VALOR RECEBIDO', 'icon' => 'money-bill', 'background' => 'pink',
+                                                'value' => 'R$ '.number_format($valor_recebido, 2,',','.') ] );
+
+            $indicador6->enableSection('main', ['title' => 'JUROS RECEBIDO', 'icon' => 'money-bill', 'background' => 'orange',
+                                                'value' => 'R$ '.number_format($juros_recebido, 2,',','.') ] );
+
+            $div->add( TElement::tag('div', $indicador1, ['class' => 'col-sm-6']) );
+            $div->add( TElement::tag('div', $indicador2, ['class' => 'col-sm-6']) );
+            $div->add( TElement::tag('div', $indicador3, ['class' => 'col-sm-6']) );
+            $div->add( TElement::tag('div', $indicador4, ['class' => 'col-sm-6']) );
+            $div->add( TElement::tag('div', $indicador5, ['class' => 'col-sm-6']) );
+            $div->add( TElement::tag('div', $indicador6, ['class' => 'col-sm-6']) );
+
+            $table1 = TTable::create( ['class' => 'table table-stripad table-home', 'style' => 'border-collape:colapse']);
+            $table1->addSection('thead');
+            $table1->addRowSet('', 'Valor', 'Valor Pago', 'Juros');
+
+            if($valor)
+            {
+                $table1->addSection('tbody');
+
+                $table1->addSection('tfoot')->style = 'color:blue';
+                $row = $table1->addRow();
+                $row->addCell('Total');
+                $row->addCell('R$' . number_format($valor, 2,',','.'))->style = 'text-align: left';
+                $row->addCell('R$' . number_format($valor_pago, 2,',','.'))->style = 'text-align: left';
+                $row->addCell('R$'.$juros)->style = 'text-align: left';
+            }
+
+            $div->add(TElement::tag('div', TPanelGroup::pack('Valor Total a Pagar', $table1),['class' => 'class-sm-6']));
+
+            $table2 = TTable::create( ['class' => 'table table-stripad table-home', 'style' => 'border-collape:colapse']);
+            $table2->addSection('thead');
+            $table2->addRowSet('', 'Valor Receber', 'Valor Recebido', 'Juros Recebido');
+
+            if($valor_receber)
+            {
+                $table2->addSection('tbody');
+
+                $table2->addSection('tfoot')->style = 'color:blue';
+                $row = $table2->addRow();
+                $row->addCell('Total Receber');
+                $row->addCell('R$' . number_format($valor_receber, 2,',','.'))->style = 'text-align: left';
+                $row->addCell('R$' . number_format($valor_recebido, 2,',','.'))->style = 'text-align: left';
+                $row->addCell('R$' . number_format($juros_recebido, 2,',','.'))->style = 'text-align: left';
+            }
+
+            $div->add(TElement::tag('div', TPanelGroup::pack('Valor Total a Receber', $table2),['class' => 'class-sm-6']));
+
+            $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+            $vbox->add($div);
         }
-
+        catch (Exception $e)
+        {
+            new TMessage('errir', $e->getMessage());
+        }
+        parent::add($vbox);
     }
 }
-
